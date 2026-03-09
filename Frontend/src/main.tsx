538b780372d75a27b05771dfd5e58f13de5b823b
CHANTREA.HAN <CHANTREA.HAN@PNC.LOCAL>
2026-03-09 15:21:55 +0700

    confused page

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import { AuthProvider } from './context/AuthContext';
@@ -9,11 +8,6 @@ import './index.css';
 
 applyInitialTheme();
 
-=======
-import App from './App';
-import './index.css';
-
->>>>>>> chantrea/feature-customer
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
