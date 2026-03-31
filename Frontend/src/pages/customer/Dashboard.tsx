b507824fc1e771387c1a5721577b07cc8f53ae23
CHANTREA.HAN <CHANTREA.HAN@PNC.LOCAL>
2026-03-31 14:51:40 +0700

    fied data in message

--- Frontend/src/pages/customer/Dashboard.tsx
+++ Frontend/src/pages/customer/Dashboard.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'motion/react';
 import { format } from 'date-fns';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { 
   Search, 
   Calendar, 
