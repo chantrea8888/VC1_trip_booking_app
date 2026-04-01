8e0d598fc7dc79c09fcd00d41afed38d176e8579
CHANTREA.HAN <CHANTREA.HAN@PNC.LOCAL>
2026-04-01 11:28:27 +0700

    setup for deploy ment

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -5,7 +5,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:5173',
-        'http://127.0.0.1:5173',
+        'https://chantreaweb.site',
         'https://vc-1-trip-booking-app-b767-cn7ehylhp-han-chantreas-projects.vercel.app',
     ],
     'allowed_origins_patterns' => [
